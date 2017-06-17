368db6c28f7edc7eac2977f9a6afe94e3f7e6ba0
Administrator <Administrator@MSDN-SPECIAL>
2017-06-17 14:20:43 +0900

    2017-06-17 Study

---
+++ test/src/jangjaehyun/r0001/ArrExam.java
@@ -0,0 +1,21 @@
+package jangjaehyun.r0001;
+
+import java.util.Scanner;
+
+public class ArrExam {
+	// 1부터 10까지 도는 반복문 작성
+	// Scannner 클래스를 이용해서 nextLine 합수를 호출하여 입력받은 값을 그대로 출력
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int[] a = new int[10];
+		String[] arrStr = new String[10];
+		for(int i = 0; i<=9; i++){
+		String str = scan.nextLine();
+		arrStr[i] = str;
+		System.out.println("a[" + i + "] = " + (str));
+		}
+		for(int i=0; i<=9; i++){
+		System.out.println(arrStr[i]);
+		}
+	}
+}
