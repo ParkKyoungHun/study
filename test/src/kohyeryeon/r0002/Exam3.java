6b475467e0a5d8d7a707426b4ed33465fb537f2c
Administrator <Administrator@MSDN-SPECIAL>
2017-06-24 16:38:03 +0900

    20170624

--- test/src/kohyeryeon/r0002/Exam3.java
+++ test/src/kohyeryeon/r0002/Exam3.java
@@ -4,26 +4,33 @@ import java.util.Scanner;
 
 public class Exam3 {
 	
+	Scanner scan = new Scanner(System.in);
 	
+	public int getIntFormString(){
+		String str = scan.nextLine();
+		try{
+			return Integer.parseInt(str);
+		}catch(Exception e){
+			System.out.println("숫자를 입력해주게!!!!");
+			return getIntFormString();
+		}
+	}
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-		String str1 = scan.nextLine();
-		int initNum = Integer.parseInt(str1);
-		String str2 = scan.nextLine();
-		int maxNum = Integer.parseInt(str2);
 		
+		Scanner scan = new Scanner(System.in);
+		Exam3 e3 = new Exam3();
+		System.out.println("초기값 입력해!");
+		int initNum = e3.getIntFormString();
+		System.out.println("맥스값 입력해!");
+		int maxNum = e3.getIntFormString();
 		
 		String[] str = new String[(maxNum-initNum)+1];
-				
-		for(int i=initNum;i<=str.length;i++){
-			str[i-initNum] = i+""; 
 			
-		}
-		
+		for(int i=initNum;i<=str.length;i++){
+				str[i-initNum] = scan.nextLine();
+		}	
 		for(int i=0;i<str.length;i++){
-			System.out.println(str[i]);
+				System.out.println(str[i]);
 		}
-		
-		
 	}
 }
