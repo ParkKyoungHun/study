a28f7a5e24f675ded0f1eeabe3dd96cc4205369b
Administrator <Administrator@MSDN-SPECIAL>
2017-06-24 16:37:08 +0900

    t

---
+++ test/src/kyounghunpark/r0002/Exam4.java
@@ -0,0 +1,16 @@
+package kyounghunpark.r0002;
+
+public class Exam4 {
+	String str;
+	int a;
+	Exam4(){
+		str = new String("str");
+		a =3;
+	}
+	
+	public static void main(String[] args){
+		Exam4 e = new Exam4();
+//		System.out.println(e.str);
+		System.out.println(e.a);
+	}
+}
