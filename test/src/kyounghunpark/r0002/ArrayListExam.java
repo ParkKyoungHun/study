fc335c1a529519b10a3072e63167877afd16296e
Administrator <Administrator@MSDN-SPECIAL>
2017-06-24 12:19:39 +0900

    t

---
+++ test/src/kyounghunpark/r0002/ArrayListExam.java
@@ -0,0 +1,9 @@
+package kyounghunpark.r0002;
+
+public class ArrayListExam {
+	// ArrayListExam에 멤버변수로 ArrayList변수를 하나 생성해주세요.
+	// 변수명은 list이며 초기화는 ArrayListExam 생성자에서 해주세요.
+	// Scanner클래스의 nextLine()함수를 사용하여 반복문 10번이 돌때까지
+	// 멤버변수 list에 값을 넣어주신후
+	// 다시 반복문을 하나 더 만들어 list값을 출력해주세요.
+}
