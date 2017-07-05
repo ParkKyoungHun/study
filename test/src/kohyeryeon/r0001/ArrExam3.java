3b02f9dab86c9304157b2830cc87fe971b704699
kohyeryeon <rhofus17@naver.com>
2017-07-05 18:36:10 +0900

    20170705

--- test/src/kohyeryeon/r0001/ArrExam3.java
+++ test/src/kohyeryeon/r0001/ArrExam3.java
@@ -1,29 +1,31 @@
 package kohyeryeon.r0001;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class ArrExam3 {
 	
 	public static void main(String[] args){
 //		Scanner scan = new Scanner(System.in);
-		String result ="";
-	      long[] a = new long[10];
-	      for(int i=0;i<10;i++){
-	         result += (i+1);
-	         a[i] = Long.parseLong(result);
-	      }
-
-	      for(int i=0;i<10;i++){
-	         System.out.println(a[i]);
-	      }
+//		String result ="";
+//	      long[] a = new long[10];
+//	      for(int i=0;i<10;i++){
+//	         result += (i+1);          //result = "" + 1  ->   문자열 "1"이 저장됨.
+//	         a[i] = Long.parseLong(result);
+//	      }
+//
+//	      for(int i=0;i<10;i++){
+//	         System.out.println(a[i]);
+//	      }
 		
 		
+		int[] a = new int[10];
+		for(int i=1;i<=10;i++){
+			a[i-1] = (i-1)*10+1; 
+			
+		}
 		
-//		for(int i=1;i<=10;i++){
-//			a[i-1] = i; 
-//		}
-//		for(int i=0;i<a.length;i++){
-//			System.out.println(a[i]);
-//		}
+		for(int i=0;i<a.length;i++){
+			System.out.println(a[i]);
+		}
 	}
 }
